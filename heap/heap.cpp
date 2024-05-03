af5943ca10b80eb51032eb0e9aeb1df2f659c33a
rafaelChelo <rafael@chelofinance.io>
2024-05-03 18:59:30 -0400

    feat: heap fixed

--- heap/heap.cpp
+++ heap/heap.cpp
@@ -5,83 +5,84 @@ using namespace std;
 #ifndef HEAP
 #define HEAP
 
-template <class Elem>
-class IHeap<Elem>: public vector< Elem >
-{
-  public:
-    IHeap();
-    IHeap(const IHeap<Elem> &h);
-    IHeap(const vector<Elem> &a);
-    ~IHeap();
-    void insert(const Elem &e);
-    void remove();
-    Elem &head(); // retorna referencia
-    Elem head() const; // retorna copia
-    static void sort(vector< Elem > &A); // ordena A con “<“
-    void heapify(vector< Elem > &A);
-    void sink(vector< Elem > &A, int k, int n); // hundir
-  private:
-    void swim(int k); // flotar
-};
+//template <class Elem>
+//class IHeap<Elem>: public vector< Elem >
+//{
+  //public:
+    //IHeap();
+    //IHeap(const IHeap<Elem> &h);
+    //IHeap(const vector<Elem> &a);
+    //~IHeap();
+    //void insert(const Elem &e);
+    //void remove();
+    //Elem &head(); // retorna referencia
+    //Elem head() const; // retorna copia
+    //static void sort(vector< Elem > &A); // ordena A con “<“
+    //void heapify(vector< Elem > &A);
+    //void sink(vector< Elem > &A, int k, int n); // hundir
+  //private:
+    //void swim(int k); // flotar
+//};
 
 //template <class T> class Heap : public IHeap<T> {
 template <class T> class Heap {
 private:
-  vector<T> *elements;
+  vector<T> elements;
 
   int parent(int pos) {
-    return (pos - 1) / 2 >= 0 && (pos - 1) / 2 < elements->size()
+    return (pos - 1) / 2 >= 0 && (pos - 1) / 2 < elements.size()
                ? (pos - 1) / 2
                : -1;
   }
 
   int left(int pos) {
-    return 2 * pos + 1 >= 0 && 2 * pos + 1 < elements->size() ? 2 * pos + 1
+    return 2 * pos + 1 >= 0 && 2 * pos + 1 < elements.size() ? 2 * pos + 1
                                                               : -1;
   }
 
   int right(int pos) {
-    return 2 * pos + 2 >= 0 && 2 * pos + 2 < elements->size() ? 2 * pos + 2
+    return 2 * pos + 2 >= 0 && 2 * pos + 2 < elements.size() ? 2 * pos + 2
                                                               : -1;
   }
 
   void swim(int i) {
-    if (parent(i) != -1 && (*elements)[i] < (*elements)[parent(i)]) {
-      std::swap((*elements)[i], (*elements)[parent(i)]);
+    if (parent(i) != -1 && elements[i] < elements[parent(i)]) {
+      std::swap(elements[i], elements[parent(i)]);
       swim(parent(i));
     }
   }
 
   void sink(int i) {
     int mn = i;
-    if (left(i) != -1 && (*elements)[left(i)] < (*elements)[i])
+    if (left(i) != -1 && elements[left(i)] < elements[i])
       mn = left(i);
-    if (right(i) != -1 && (*elements)[right(i)] < (*elements)[mn])
+    if (right(i) != -1 && elements[right(i)] < elements[mn])
       mn = right(i);
     if (mn != i) {
-      std::swap((*elements)[i], (*elements)[mn]);
+      std::swap(elements[i], elements[mn]);
       sink(mn);
     }
   }
 
 public:
-  Heap() { elements = new vector<T>(); }
+  Heap() { }
 
   void print() {
-    while (elements->size() > 0)
+    while (elements.size() > 0)
       cout << remove();
   }
 
-  int size() { return elements->size(); }
+  int size() { return elements.size(); }
 
   void insert(const T &val) {
-    elements->pushBack(val);
-    swim(elements->size() - 1);
+    elements.push_back(val);
+    swim(elements.size() - 1);
   }
 
   T remove() {
-    T val = (*elements)[0];
-    (*elements)[0] = elements->popBack();
+    T val = elements[0];
+    elements[0] = elements[elements.size() - 1];
+    elements.pop_back();
     sink(0);
     return val;
   }
---
+++ heap/processing.cpp
@@ -0,0 +1,35 @@
+#include "heap.cpp"
+
+#include <sstream>
+#include <vector>
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+string processInput(const string& input) {
+    std::istringstream iss(input);
+    Heap<int>* heap = new Heap<int>();
+    string result = "";
+    int number;
+
+    while (iss >> number) {
+        heap->insert(number);
+    }
+
+    std::vector<int> results;
+
+    while (heap->size() > 0){
+        int el = heap->remove();
+        result += to_string(el);
+        result += " ";
+    }
+
+    result.pop_back();
+    return result;
+}
+
+bool validate(const string& output) {
+    return false; // No conflicts found
+}
+
